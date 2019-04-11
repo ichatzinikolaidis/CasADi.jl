8a90d4c2ae5025af1694fabe4616f8dc6c974a99
ichatzinikolaidis <lohatzis@gmail.com>
2019-04-11 18:25:28 +0100

    Overload hcat, vcat, size

--- src/importexport.jl
+++ src/importexport.jl
@@ -4,3 +4,9 @@ Base.adjoint(x::CasadiSymbolicObject) = getproperty(casadi, :transpose)(x)
 # trigonometric functions
 Base.cos(x::CasadiSymbolicObject) = getproperty(casadi, :cos)(x)
 Base.sin(x::CasadiSymbolicObject) = getproperty(casadi, :sin)(x)
+
+Base.hcat(x::CasadiSymbolicObject) = getproperty(casadi, :hcat)(x)
+Base.vcat(x::CasadiSymbolicObject) = getproperty(casadi, :vcat)(x)
+
+# SX specific
+Base.size(x::SX) = getproperty(casadi.SX, :size)(x)
