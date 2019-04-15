115f68c22e02f7ee69d37496befc2d59e8125189
ichatzinikolaidis <lohatzis@gmail.com>
2019-04-15 12:57:49 +0100

    Improve constructors

--- src/constructors.jl
+++ src/constructors.jl
@@ -1,10 +1,10 @@
 for i ∈ casadi_types
     @eval begin
         $i(x::$i) = x
-        $i(x::T) where {T <: Union{Real, AbstractVecOrMat}} = casadi.hcat([casadi.vcat(i) for i in eachcol(x)])
+        $i(x::T) where {T <: Union{Real, AbstractVecOrMat}} = casadi.$i(x)
 
         $i(x::AbstractString) = casadi.$i.sym(x)
-        $i(x::AbstractString, i1::Integer) = casadi.$i.sym(x, i1, 1)
+        $i(x::AbstractString, i1::Integer) = casadi.$i.sym(x, i1)
         $i(x::AbstractString, i1::Integer, i2::Integer) = casadi.$i.sym(x, i1, i2)
         convert(::Type{$i}, s::AbstractString) = $i(s)
 
