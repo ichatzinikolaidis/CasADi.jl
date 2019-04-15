69a3fb92319f93fb8f18ce68a264c8d155929dff
ichatzinikolaidis <lohatzis@gmail.com>
2019-04-15 12:57:36 +0100

    Add range and colon indexing support

--- src/generic.jl
+++ src/generic.jl
@@ -1,27 +1,13 @@
 for i ∈ casadi_types
     @eval begin
-        ## Iterator
-        import Base.iterate
-        iterate(x::$i) = (x.x, 0)
-        iterate(x::$i, state) = nothing
-
         ## Indexing
-        function Base.getindex(x::$i, j::Int)
-            if j <= 0 || j > casadi.$i.numel(x)
-                throw( BoundsError(x, j) )
-            end
-            item = x.x.__getitem__(j-1)
-        end
-        function Base.getindex(x::$i, j1::Int, j2::Int)
-            if j1 <= 0 || j1 > casadi.$i.size1(x)
-                throw( BoundsError(x, (j1,j2)) )
-            end
-            if j2 <= 0 || j2 > casadi.$i.size2(x)
-                throw( BoundsError(x, (j1,j2)) )
-            end
-            item = x.x.__getitem__( j1-1 + (j2-1)*casadi.$i.size1(x) )
-        end
+        Base.getindex(x::$i, j::Union{Int, UnitRange{Int}, Colon}) =
+            x.x.__getitem__([1:length(x)...][j] .- 1)
 
+        Base.getindex(x::$i, j1::Union{Int, UnitRange{Int}, Colon}, j2::Union{Int, UnitRange{Int}, Colon}) =
+            x.x.__getitem__(LinearIndices( size(x) )[j1,j2] .- 1)
+
+        # Last index
         Base.lastindex(x::$i) = casadi.$i.numel(x)
         function Base.lastindex(x::$i, j::Int)
             if j == 1
@@ -53,5 +39,7 @@ for i ∈ casadi_types
         ## Adjoint and transpose
         Base.adjoint(x::$i) = casadi.transpose(x)
         Base.transpose(x::$i) = casadi.transpose(x)
+
+        Base.length(x::$i) = casadi.$i.numel(x)
     end
 end
