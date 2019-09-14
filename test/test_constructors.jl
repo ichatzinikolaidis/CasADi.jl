7b7ae3f448052642137dfa9b5b84dd5b17a5e1a1
Iordanis Chatzinikolaidis <lohatzis@gmail.com>
2019-09-14 15:57:22 +0100

    Fix number of spaces

--- test/test_constructors.jl
+++ test/test_constructors.jl
@@ -1,38 +1,38 @@
 @testset "Construct constant $i from scalar, Vector, Matrix" for i ∈ cas_symbol
-  @eval begin
-    scalar1 = $i(1)
-    scalar2 = $i(1.0)
-    V = $i([1;2;3])
-    M = $i([1 2 ; 3 4 ; 5 6])
+    @eval begin
+        scalar1 = $i(1)
+        scalar2 = $i(1.0)
+        V = $i([1;2;3])
+        M = $i([1 2 ; 3 4 ; 5 6])
 
-    @test casadi.is_equal(casadi.$i(1), scalar1, 1)
-    @test casadi.is_equal(casadi.$i(1.0), scalar2, 1)
-    @test casadi.is_equal(scalar1, scalar2, 1)
-    @test casadi.is_equal(casadi.$i([1;2;3]), V, 1)
-    @test casadi.is_equal(casadi.$i([1 2 ; 3 4 ; 5 6]), M, 1)
-  end
+        @test casadi.is_equal(casadi.$i(1), scalar1, 1)
+        @test casadi.is_equal(casadi.$i(1.0), scalar2, 1)
+        @test casadi.is_equal(scalar1, scalar2, 1)
+        @test casadi.is_equal(casadi.$i([1;2;3]), V, 1)
+        @test casadi.is_equal(casadi.$i([1 2 ; 3 4 ; 5 6]), M, 1)
+    end
 end
 
 @testset "Construct variable $i vector and matrix          " for i ∈ cas_symbol
-  @eval begin
-    v = casadi.$i.sym("v",3)
-    V = $i("V",3)
-    v_val = rand(3)
-    @test casadi.is_equal(casadi.substitute(v,v,v_val), casadi.substitute(V,V,v_val), 1)
+    @eval begin
+        v = casadi.$i.sym("v",3)
+        V = $i("V",3)
+        v_val = rand(3)
+        @test casadi.is_equal(casadi.substitute(v,v,v_val), casadi.substitute(V,V,v_val), 1)
 
-    m = casadi.$i.sym("m",2,4)
-    M = $i("M",2,4)
-    m_val = rand(2,4)
-    @test casadi.is_equal(casadi.substitute(m,m,m_val), casadi.substitute(M,M,m_val), 1)
-  end
+        m = casadi.$i.sym("m",2,4)
+        M = $i("M",2,4)
+        m_val = rand(2,4)
+        @test casadi.is_equal(casadi.substitute(m,m,m_val), casadi.substitute(M,M,m_val), 1)
+    end
 end
 
 @testset "AbstractFloat and Integer constructors           " for i ∈ cas_symbol
-  @eval begin
-    af = [1.2 ; -0.0 ; -4.3]
-    @test casadi.is_equal($i(af)', casadi.$i(af)', 2)
+    @eval begin
+        af = [1.2 ; -0.0 ; -4.3]
+        @test casadi.is_equal($i(af)', casadi.$i(af)', 2)
 
-    int = [1 ; 0 ; -2]
-    @test casadi.is_equal($i(int)', casadi.$i(int)', 2)
-  end
+        int = [1 ; 0 ; -2]
+        @test casadi.is_equal($i(int)', casadi.$i(int)', 2)
+    end
 end
--- test/test_generic.jl
+++ test/test_generic.jl
@@ -1,15 +1,15 @@
 @testset "Transform $i to null, scalar, Vector, and Matrix " for i ∈ cas_symbol
-  @eval begin
-    null_symbol = $i("null_symbol", 0)
-    scalar = $i("scalar", 1)
-    V = $i("V1", 3) - $i("V2", 3)
-    M = $i("M1", 2, 4) - $i("M2", 2, 4)
+    @eval begin
+        null_symbol = $i("null_symbol", 0)
+        scalar = $i("scalar", 1)
+        V = $i("V1", 3) - $i("V2", 3)
+        M = $i("M1", 2, 4) - $i("M2", 2, 4)
 
-    @test Vector(null_symbol) == Vector{Any}()
-    @test Matrix(null_symbol) == Vector{Any}(undef,0)
-    @test casadi.is_equal($i( Vector(scalar) ), scalar)
-    @test casadi.is_equal($i( Matrix(scalar) ), scalar)
-    @test casadi.is_equal($i( Vector(V) ), V, 1)
-    @test casadi.is_equal($i( Matrix(M) ), M, 1)
-  end
+        @test Vector(null_symbol) == Vector{Any}()
+        @test Matrix(null_symbol) == Vector{Any}(undef,0)
+        @test casadi.is_equal($i( Vector(scalar) ), scalar)
+        @test casadi.is_equal($i( Matrix(scalar) ), scalar)
+        @test casadi.is_equal($i( Vector(V) ), V, 1)
+        @test casadi.is_equal($i( Matrix(M) ), M, 1)
+    end
 end
