5eb05cb8f1a3472138228ccf755f126f9c6a5793
Iordanis Chatzinikolaidis <lohatzis@gmail.com>
2020-10-23 17:08:13 +0100

    Fix sincos test

--- test/test_mathfuns.jl
+++ test/test_mathfuns.jl
@@ -2,7 +2,7 @@ function test_mathfuns(::Type{T}) where T <: CasadiSymbolicObject
     @testset "$( string("Math functions for ", T, "                            ") )" begin
         x = randn()
 
-        @test to_julia.( sincos( T(x) ) ) == sincos(x)
+        @test all(to_julia.( sincos( T(x) ) ) .≈ sincos(x))
         @test to_julia( sinc( T(x) ) )    ≈ sinc(x/π)
         @test to_julia( abs( T(x) ) )     ≈ abs(x)
     end
